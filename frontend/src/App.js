b0f88a2b64b1d3a1c7da6ea61760ee4b3799b104
MelissaPatez <m.patez@teltecsolutions.com.br>
2022-02-01 10:43:52 -0300

    applying react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,27 @@ import React from 'react'
 //global style
 import GlobalStyle from './components/GlobalStyle'
 
+//Roteamento
+import { BrowserRouter, Router, Route, Routes} from 'react-router-dom'
+
 //component
 import AboutUs from './pages/AboutUs'
 
 
+//switch --> routes
 function App() {
   return (
     <div>
       <GlobalStyle/>
-      <AboutUs/>
+
+     <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AboutUs/>} />
+        
+        </Routes>
+     
+     </BrowserRouter>
+      
 
     </div>
   );
