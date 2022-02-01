1bbc4a098981dcba191fc7f2a0fe5296878e218a
MelissaPatez <m.patez@teltecsolutions.com.br>
2022-02-01 14:44:19 -0300

    including styled in contactme page

--- frontend/src/pages/ContactMe.js
+++ frontend/src/pages/ContactMe.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import styled from 'styled-components'//css global
 
 const ContactMe = () => {
     return (
-        <div>
-            <div> Get in touch</div>
-            <div id="areas">
+        <ContactStyled>
+            <Title > 
+                <h2>Get in touch</h2>
+            </Title >
+            <Areas>
                 <div id="form">
                     <form id="contact-form" method='post'>
                         Name: <input  type="text"/>
@@ -13,20 +16,59 @@ const ContactMe = () => {
                         <button type="submit">Submit</button>
                     </form>
                 </div>
-                <div id="network">
-                    <div className='social'>
+                <Network>
+                    
+                    <Social href='https://github.com/MelissaPatez' target='_blank'>
                         icon
                         <h3>github.com/MelissaPatez</h3>
-                    </div>
-                    <div className='social'>
+                    </Social>
+
+                    <Social>
                         icon
                         <h3>LinkedIn</h3>
-                    </div>
-              
-                </div>
-            </div>
-        </div>
+                    </Social>
+                    
+                </Network>
+            </Areas>
+        </ContactStyled>
     )
 }
+
+const ContactStyled = styled.div`
+    padding: 1rem 10rem;
+    color: #353535;
+    min-height: 90vh;
+`;
+
+const Title = styled.div`
+   margin-bottom: 4rem;
+   h2{
+       color: white;
+   }
+`;
+
+const Areas = styled.div`
+   display: flex;
+   align-items: center;
+`;
+
+const Network = styled.div`
+    padding: 1rem 5rem;
+`;
+
+// a é link  target='_blank' abre em outra janela
+const Social = styled.a`
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+    border-radius: 15px 50px;
+    padding-left: 2rem;
+    text-decoration: none;
+    &:hover{
+        background: #7a7e85;
+        cursor: pointer; 
+    }
+`;
 
 export default ContactMe
