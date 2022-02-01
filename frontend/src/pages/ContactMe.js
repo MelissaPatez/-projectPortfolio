4a98cc91767a0c1a0a806b7d78731d0ac57a3534
MelissaPatez <m.patez@teltecsolutions.com.br>
2022-02-01 14:20:56 -0300

    ContactMe page under development

--- frontend/src/pages/ContactMe.js
+++ frontend/src/pages/ContactMe.js
@@ -2,7 +2,30 @@ import React from 'react'
 
 const ContactMe = () => {
     return (
-        <div>Contact Me</div>
+        <div>
+            <div> Get in touch</div>
+            <div id="areas">
+                <div id="form">
+                    <form id="contact-form" method='post'>
+                        Name: <input  type="text"/>
+                        Email: <input  type="email"/>
+                        Message: <textarea rows='5'/>
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
+                <div id="network">
+                    <div className='social'>
+                        icon
+                        <h3>github.com/MelissaPatez</h3>
+                    </div>
+                    <div className='social'>
+                        icon
+                        <h3>LinkedIn</h3>
+                    </div>
+              
+                </div>
+            </div>
+        </div>
     )
 }
 
