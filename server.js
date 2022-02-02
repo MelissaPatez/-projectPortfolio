966bba5bafd13b2dee54be484ac4abbb5465cacc
MelissaPatez <m.patez@teltecsolutions.com.br>
2022-02-02 20:52:19 -0300

    installing the build

--- server.js
+++ server.js
@@ -14,6 +14,19 @@ const PORT = process.env.PORT;
 
 app.use('/api', api)
 
+
+if (process.env.NODE_ENV === 'production' ) {
+	//Express vai entregar os assets de produção
+	// Como por exemplo: man.js ou main.css
+	app.use(express.static('frontend/build'));
+	
+	//Express vai entregar o index.html, se não reconhecer a rota 
+	const path =  require('path')
+	app.get('*', ( req, res ) => {
+		res.sendFile(path.resolve(__dirname, 'frontend', 'buil', 'index.html'))
+	})
+}
+
 // rota front end  --> /api 
 app.use(express.urlencoded({ extended: false }));
 app.listen(PORT); 
